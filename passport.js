9e329e10a26ebe38be3ff9010030a9b0f5d19210
Vivek <vivekdhiman1208@gmail.com>
2023-07-08 10:33:21 +0530

    Passport changes

--- passport.js
+++ passport.js
@@ -8,7 +8,7 @@ passport.use(
         {
             clientID: process.env.CLIENT_ID,
             clientSecret: process.env.CLIENT_SECRET,
-            callbackURL: "https://genletter-backend-b8q3.onrender.com/auth/google/callback",
+            callbackURL: "/auth/google/callback",
             scope: ["profile", "email"],
         },
         function (accessToken, refreshToken, profile, callback) {
